9bbb3b604dc2205077429c5f675b5999ac48a3ac
rsurr <rsurracow@gmail.com>
2024-04-17 19:40:56 -0300

    Delta weight

--- Data preparation/6. X_imae.R
+++ Data preparation/6. X_imae.R
@@ -443,6 +443,70 @@ X_imae <-
   left_join(turnos, by=c("CAIMAE", "mes_solicitud")) %>% 
   mutate(CAIMAE=as.factor(CAIMAE))
 
+# Delta peso ----
+
+dP <- SESIONES_HD %>%
+  rename(ZCAIMAE=ZPMD_IMAE) %>% 
+  mutate(DPESOSE=case_when(DPESOSE==999.00 ~ NA,
+                           .default = as.numeric(DPESOSE)),
+         DPESOPO=case_when(DPESOPO==999.00 ~ NA,
+                           .default = as.numeric(DPESOPO)),
+         dife_post=DPESOPO-DPESOSE,
+         dife_pre=DPESOPR-DPESOSE,
+         peso=case_when(abs(dife_post)<=0.5 ~ 1,
+                        abs(dife_post)>0.5 ~ 0,
+                        is.na(dife_post) ~ NA)) %>% 
+  select(peso, CAPACNUM, DPESOPR, DPESOPO, ZCAIMAE, PMD_ANIO, dife_post, 
+         dife_pre)
+
+# Reorder "anio" in increasing order
+dP$anio <- factor(dP$PMD_ANIO, levels = sort(unique(dP$PMD_ANIO)))
+
+# Reorder "ZCAIMAE" alphabetically
+dP$ZCAIMAE <- factor(dP$ZCAIMAE, levels = sort(unique(dP$ZCAIMAE)))
+
+# Set reference levels for anio, ZCAIMAE and CAPACNUM
+base$anio <- relevel(base$anio, ref = "2004")
+base$ZCAIMAE <- relevel(base$ZCAIMAE, ref = "ASOCIACION ESPAÑOLA")
+base$CAPACNUM <- relevel(base$CAPACNUM, ref="344677")
+
+dP <- dP %>% unite("ZCAIMAEanio", ZCAIMAE:PMD_ANIO, sep=":", remove = FALSE) 
+
+m_dP1 <- feols(peso ~ DPESOPR | ZCAIMAEanio + CAPACNUM, dP)
+m_dP2 <- feols(peso ~ dife_pre | ZCAIMAEanio + CAPACNUM, dP)
+m_dP3 <- feols(dife_post ~ DPESOPR | ZCAIMAEanio + CAPACNUM, dP)
+m_dP4 <- feols(dife_post ~ dife_pre | ZCAIMAEanio + CAPACNUM, dP)
+m_dP5 <- feols(DPESOPO ~ DPESOPR | ZCAIMAEanio + CAPACNUM, dP)
+
+delta_p1 <- fixef(m_dP1)$ZCAIMAEanio %>% as.data.frame() %>% 
+  rownames_to_column("ZCAIMAEanio")
+colnames(delta_p1) <- c("ZCAIMAEanio", "delta_p1")
+
+delta_p2 <- fixef(m_dP2)$ZCAIMAEanio %>% as.data.frame() %>% 
+  rownames_to_column("ZCAIMAEanio")
+colnames(delta_p2) <- c("ZCAIMAEanio", "delta_p2")
+
+delta_p3 <- fixef(m_dP3)$ZCAIMAEanio %>% as.data.frame() %>% 
+  rownames_to_column("ZCAIMAEanio")
+colnames(delta_p3) <- c("ZCAIMAEanio", "delta_p3")
+
+delta_p4 <- fixef(m_dP4)$ZCAIMAEanio %>% as.data.frame() %>% 
+  rownames_to_column("ZCAIMAEanio")
+colnames(delta_p4) <- c("ZCAIMAEanio", "delta_p4")
+
+delta_p5 <- fixef(m_dP5)$ZCAIMAEanio %>% as.data.frame() %>% 
+  rownames_to_column("ZCAIMAEanio")
+colnames(delta_p5) <- c("ZCAIMAEanio", "delta_p5")
+
+delta_p <- delta_p1 %>% 
+  left_join(delta_p2, join_by("ZCAIMAEanio")) %>%
+  left_join(delta_p3, join_by("ZCAIMAEanio")) %>%
+  left_join(delta_p4, join_by("ZCAIMAEanio")) %>%
+  left_join(delta_p5, join_by("ZCAIMAEanio")) %>%
+  separate(ZCAIMAEanio, sep = ":", into = c("ZCAIMAE", "anio")) %>% 
+  mutate(anio=as.double(anio))
+
+
 # Logit_INGRESOS ----
 
 Logit_INGRESOS <- 
@@ -463,11 +527,13 @@ Logit_INGRESOS <-
   left_join(X_imae, by=c("ID_CAIMAE"="CAIMAE", "mes_solicitud")) %>% 
   left_join(MEDICOS, by=c("ZB1SMEDIC"="ZB1RMEDICO", "anio_solicitud"="PMDANIO")) %>%
   left_join(quality, by=c("ZCAIMAE", "anio_solicitud"="anio")) %>% 
+  left_join(delta_p, by=c("ZCAIMAE", "anio_solicitud"="anio")) %>% 
   mutate(inst=case_when(ZCASINST==ZCASINST_IMAE~1,
                         is.na(ZCASINST_IMAE)~0,
                               .default = 0),
          medimae = ifelse(rowSums(select(., starts_with("medimae")) == num_choice, na.rm = TRUE) > 0, 1, 0)) %>% 
   select(-c(matches("^medimae[0-9]+$")))
+
 
 write_dta(Logit_INGRESOS, 
           "C:/Users/julie/OneDrive/Documentos/Proyecto Tesis/MastersThesis/Logit_INGRESOS.dta",
